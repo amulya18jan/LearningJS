2d5fc948589264ed9fab1abdd74237242fcd43ca
amulya18jan <amulyaawasth18jan@gmail.com>
2026-06-19 11:56:04 +0530

    Learning JS

---
+++ Introduction/index.js
@@ -0,0 +1,10 @@
+console.log((4+9+6)/77);
+let a= 10;
+a=12;
+console.log(a);
+let b = 7*a;
+console.log(b);
+const max =57;
+const actual=max-13;
+const percentage = actual/max;
+console.log(percentage);
