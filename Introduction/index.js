a228044578374dcaecd09d2e57982b8df0a0b943
Amulya Awasthi <amulyaawasth18jan@gmail.com>
2026-06-20 22:50:07 +0530

    Learning String in JS

--- Introduction/index.js
+++ Introduction/index.js
@@ -1,12 +1,15 @@
 console.log((4+9+6)/77);
 //var a= 10;
 a =12;
-const a =9;
+// const a =9;
 
-console.log(a);
-let b = 7*a;
-console.log(b);
-const max =57;
-const actual=max-13;
-const percentage = actual/max;
-console.log(percentage);
+// console.log(a);
+// let b = 7*a;
+// console.log(b);
+// const max =57;
+// const actual=max-13;
+// const percentage = actual/max;
+// console.log(percentage);
+let str = 'Hello,';
+let str1 ="World";
+console.log (`${str} ${str1}`);
