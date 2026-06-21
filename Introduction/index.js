b227da1a90944e436b35bacb3adcdf78a2cc70fa
Amulya Awasthi <amulyaawasth18jan@gmail.com>
2026-06-21 19:11:04 +0530

    Learning String in js with its methods

--- Introduction/index.js
+++ Introduction/index.js
@@ -12,5 +12,21 @@ a =12;
 // console.log(percentage);
 let str = 'Hello,';
 let str1 ="World";
+console.log(`String Length : -`)
 console.log (`${str} ${str1}`); 
 console.log(str.length);
+console.log(`String charAt`);
+let str3 ="Aba jaba daba khaba (rabba";
+console.log(str3.charAt(4))
+console.log(`String indexOf and lastIndexOf`);
+let str4 ="The code is undefined code code !";
+console.log(str4.indexOf("c" , 4));
+console.log(str4.lastIndexOf("c", 24));
+console.log(str4.indexOf(""));
+console.log(str4.lastIndexOf(""));
+console.log(str4.length);
+console.log("String slice and substring");
+let str5 ="The morning is upon us!";
+console.log(str5.length);
+console.log(str5.slice(4,6));
+console.log(str5.substring(24,1));
