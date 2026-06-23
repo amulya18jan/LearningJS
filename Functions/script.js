2ede462ae2e4a02fa3ba0afe59c5feee83531d7b
Amulya Awasthi <amulyaawasth18jan@gmail.com>
2026-06-23 23:21:58 +0530

    Learning default parameters of function

--- Functions/script.js
+++ Functions/script.js
@@ -15,3 +15,14 @@ const addTwoNum = function (a,b) {
   return (a+b);
 };
 console.log (addTwoNum(10,20));
+
+console.log("______________________________________________________________________");
+console.log("Default parameters in functions of java scripts");
+function DEF(a , b=0){
+ return (2*(a+b));
+}
+console.log("Giving all parameters");
+console.log(DEF(10,10));
+console.log("Giving only one parameters");
+console.log(DEF(10));
+console.log("______________________________________________________________________");
