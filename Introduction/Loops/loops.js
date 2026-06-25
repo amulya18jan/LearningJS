3abf5789fb3d9bb5f5269dc9747aa5b2905dc8d6
Amulya Awasthi <amulyaawasth18jan@gmail.com>
2026-06-25 23:00:00 +0530

    Learning for of loop in java script

--- Introduction/Loops/loops.js
+++ Introduction/Loops/loops.js
@@ -17,10 +17,17 @@ console.log("Table of any number");
 //     console.log(result);
 //     i++;
 // }
-let i=0;
-do{
-    console.log(i);
-    i++;
-}while(i<10);
+// let i=0;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<10);
+let str = "Amulya Awasthi";
+let length = 0;
+for(val of str){
+    console.log("i" + "=" + val);
+    length++;
+}
+console.log(length);
 
 
