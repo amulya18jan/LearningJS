c9549ec01d9f6543005e8fb9b985685f677808f7
Amulya Awasthi <amulyaawasth18jan@gmail.com>
2026-06-25 22:39:11 +0530

    Learning Loops

---
+++ Introduction/Loops/loops.js
@@ -0,0 +1,19 @@
+console.log("Learning all types of loops");
+console.log("Learning for loop");
+// for(let i =0;i<=10;i++){
+//     console.log(i);
+// }
+console.log("Table of any number");
+// let num = parseInt(prompt("Enter a number of which you want table"));
+// for(let i =1; i<=20;i++){
+//     let result = num * i;
+//     console.log(`${num}*${i} = ${result}`);
+// }
+let num =15;
+let i =1;
+while(i<=20){
+    let result = num * i;
+    console.log(result);
+    i++;
+}
+
