c94baf03054e638939168195529ca8302ca01e1c
Amulya Awasthi <amulyaawasth18jan@gmail.com>
2026-06-28 23:47:30 +0530

    Learning forEach() and solving questions

--- Introduction/Arrays/Arrays.js
+++ Introduction/Arrays/Arrays.js
@@ -8,42 +8,62 @@
 // const data =[1,2,3,4,5,6,7];
 // const dataMap = data.map((any)=>any+any);
 // console.log(dataMap);
-console.log("1. Print all fruits using forEach()");
-const fruits = ["Apple", "Banana", "Mango", "Orange"];
-const fruits1 = fruits.forEach((items)=>{console.log(items)})
-console.log("______________________________________________________________________");
-console.log("2. Print squares using forEach()");
-const numbers = [2, 4, 6, 8];
-const squares = numbers.forEach((sqr)=>{
-    console.log(sqr*sqr);
-})
-console.log("______________________________________________________________________");
-console.log("3. Print even and odd");
-const number1 = [1, 2, 3, 4, 5, 6];
-const evenAndOdd = number1.forEach((num) => {
-    if(num %2 == 0){
-        console.log("Even Numbers :"+" "+ num);
+// console.log("1. Print all fruits using forEach()");
+// const fruits = ["Apple", "Banana", "Mango", "Orange"];
+// const fruits1 = fruits.forEach((items)=>{console.log(items)})
+// console.log("______________________________________________________________________");
+// console.log("2. Print squares using forEach()");
+// const numbers = [2, 4, 6, 8];
+// const squares = numbers.forEach((sqr)=>{
+//     console.log(sqr*sqr);
+// })
+// console.log("______________________________________________________________________");
+// console.log("3. Print even and odd");
+// const number1 = [1, 2, 3, 4, 5, 6];
+// const evenAndOdd = number1.forEach((num) => {
+//     if(num %2 == 0){
+//         console.log("Even Numbers :"+" "+ num);
     
-    }else{
-        console.log("Odd"+ " "+ num);
-    }
-})
+//     }else{
+//         console.log("Odd"+ " "+ num);
+//     }
+// })
+// console.log("______________________________________________________________________");
+// console.log("4. Calculate total sum");
+// const add =[10, 20, 30, 40];
+// let result =0;
+// const sum = add.forEach((addAll)=>{
+//     result += addAll;
+    
+// })
+// console.log(result);
+// console.log("______________________________________________________________________");
+// console.log("5. Print student information");
+// const students = [
+//     { name: "John", age: 20 },
+//     { name: "Alice", age: 22 },
+//     { name: "Bob", age: 19 }
+// ];
+// const print = students.forEach((items)=>{
+//     console.log(`${items.name} is ${items.age} old`);
+// })
 console.log("______________________________________________________________________");
-console.log("4. Calculate total sum");
-const add =[10, 20, 30, 40];
-let result =0;
-const sum = add.forEach((addAll)=>{
-    result += addAll;
+console.log(("Learning map() and solving questions"));
+
+console.log("1. Double every number");
+const numbers = [1, 2, 3, 4, 5];
+const multiple2=numbers.map((num)=>{
+ return num*2;
     
 })
-console.log(result);
+console.log(multiple2);
+
 console.log("______________________________________________________________________");
-console.log("5. Print student information");
-const students = [
-    { name: "John", age: 20 },
-    { name: "Alice", age: 22 },
-    { name: "Bob", age: 19 }
-];
-const print = students.forEach((items)=>{
-    console.log(`${items.name} is ${items.age} old`);
+
+console.log("2. Convert to uppercase");
+const names = ["john", "alice", "bob"];
+const upperCaseNames= names.map((upperCase)=>{
+    return upperCase.toUpperCase();
+    
 })
+console.log(upperCaseNames);
