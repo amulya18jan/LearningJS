a61a463f66c499fe1cdd83f4d456ad4976e820ba
Amulya Awasthi <amulyaawasth18jan@gmail.com>
2026-06-28 22:14:00 +0530

    Learning forEach() and solving questions

--- Introduction/Arrays/Arrays.js
+++ Introduction/Arrays/Arrays.js
@@ -1,10 +1,18 @@
-console.log("Learning first method in Arrays 1. forEach()");
-let food =["Tomato", "Apple", "Rice","Pulses"];
- food.forEach((items) => {
-    console.log(items); ;
+// console.log("Learning first method in Arrays 1. forEach()");
+// let food =["Tomato", "Apple", "Rice","Pulses"];
+//  food.forEach((items) => {
+//     console.log(items); ;
+// })
+// console.log("___________________________________________________________");
+// console.log("Learning map() method in js");
+// const data =[1,2,3,4,5,6,7];
+// const dataMap = data.map((any)=>any+any);
+// console.log(dataMap);
+console.log("1. Print all fruits using forEach()");
+const fruits = ["Apple", "Banana", "Mango", "Orange"];
+const fruits1 = fruits.forEach((items)=>{console.log(items)})
+console.log("2. Print squares using forEach()");
+const numbers = [2, 4, 6, 8];
+const squares = numbers.forEach((sqr)=>{
+    console.log(sqr*sqr);
 })
-console.log("___________________________________________________________");
-console.log("Learning map() method in js");
-const data =[1,2,3,4,5,6,7];
-const dataMap = data.map((any)=>any+any);
-console.log(dataMap);
