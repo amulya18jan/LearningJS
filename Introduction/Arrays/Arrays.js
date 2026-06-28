650fa044f51c2e5d21d9a45ec20d7c30c74fdd5a
Amulya Awasthi <amulyaawasth18jan@gmail.com>
2026-06-28 21:21:28 +0530

    Learning Arrys Method in js

---
+++ Introduction/Arrays/Arrays.js
@@ -0,0 +1,10 @@
+console.log("Learning first method in Arrays 1. forEach()");
+let food =["Tomato", "Apple", "Rice","Pulses"];
+ food.forEach((items) => {
+    console.log(items); ;
+})
+console.log("___________________________________________________________");
+console.log("Learning map() method in js");
+const data =[1,2,3,4,5,6,7];
+const dataMap = data.map((any)=>any+any);
+console.log(dataMap);
