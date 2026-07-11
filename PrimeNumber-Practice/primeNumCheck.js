50bf60cc3ec8b603d66cb0c7d1272044ebb8d3f6
amulya18jan <amulyaawasth18jan@gmail.com>
2026-07-11 12:55:03 +0530

    Learning whenther the num is prime or not

---
+++ PrimeNumber-Practice/primeNumCheck.js
@@ -0,0 +1,18 @@
+console.log("1. Write a function that checks if a number is prime");
+let num =Number(prompt("Give me a number and i will check whether its prime or not :"));
+const isPrime=(num)=>{
+    if(num < 2){
+        return false;
+    }
+    for(let i=2;i*i<=num;i++){
+        if(num%i===0){
+            return false
+        }
+        return true;
+    }
+}
+if(isPrime(num)){
+    console.log(num +" " +"Its a prime number")
+}else{
+    console.log(num +" " + "Its noot a prime number");
+}
