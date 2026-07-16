293060f51967b0ae54eee208157dfcedff89c54f
Amulya Awasthi <amulyaawasth18jan@gmail.com>
2026-07-16 20:37:58 +0530

    Solving Prime number in objects

--- Object-Practice/obj.js
+++ Object-Practice/obj.js
@@ -6,6 +6,30 @@
 // console.log(newObject["Date of birth"]);
 // console.log(newObject);
 const primeObject ={
+    nPrimeNUmber(num){
+     if(num<2){
+        
+        return "O or 1 are not the Prime numbers";
+     }
+     let Primes =[]
+     let count =2;
+     while(count<=num){
+        let prime =true;
+        for(let i=2;i*i<=count;i++){
+            if(count % i===0){
+                prime =false;
+                break;
+            }
+          
+        }
+         if(prime){
+            Primes.push(count);
+        } 
+        
+        count++;
+     }
+     return Primes;
+    },
     isPrime(num){
      if(num<2){
         return false;
@@ -31,4 +55,6 @@ const primeObject ={
         return TwinPrime;
     }
 }
+console.log(primeObject.nPrimeNUmber(20));
+console.log(primeObject.isPrime(20));
 console.log(primeObject.isTwinPrime(20));
