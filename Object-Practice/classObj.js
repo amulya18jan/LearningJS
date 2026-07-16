aab8a5abdf48624312f83ae8722b336d8cad73d4
Amulya Awasthi <amulyaawasth18jan@gmail.com>
2026-07-16 23:07:01 +0530

    done changes in class

--- Object-Practice/classObj.js
+++ Object-Practice/classObj.js
@@ -14,6 +14,15 @@ isPrime(num){
       return true;
     
     }
+    sexyPrime(num){
+        const sexyP=[];
+        for(let i =2;i<num-6;i++){
+            if(this.isPrime(i)&& this.isPrime(i+6)){
+                sexyP.push([i, i+6]);
+            }
+        }
+        return sexyP;
+    }
     isTwinPrime(num){
         const TwinPrime=[];
         for(let i =2;i<num-2;i++){
@@ -27,6 +36,7 @@ isPrime(num){
 }
 let primeCheck =new NumFinder();
 let TwinPrime = new NumFinder();
+let SexyPrime =new NumFinder();
 console.log(primeCheck.isPrime(7));
 console.log(TwinPrime.isTwinPrime(20));
-console.log(typeof NumFinder);
+console.log(SexyPrime.sexyPrime(50));
