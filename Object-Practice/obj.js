080dbce150155f54dfb257e5997d664ce647ea81
amulya18jan <amulyaawasth18jan@gmail.com>
2026-07-20 10:17:56 +0530

    Learning obj value

--- Object-Practice/obj.js
+++ Object-Practice/obj.js
@@ -87,3 +87,13 @@ for(let key in obj){
     console.log(key+ ":" + obj[key])
     
 }
+let codes ={
+    "+49":"Germany",
+    "+41": "Switerland",
+    "+44": "Great Britain",
+    "+1": "USA",
+
+}
+for(let code in codes){
+    console.log(+code + ":"+ codes[code]);
+}
