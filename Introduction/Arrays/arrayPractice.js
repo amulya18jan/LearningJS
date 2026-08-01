4580ae09d8cab050a894667a65c9a82a228495f7
Amulya Awasthi <amulyaawasth18jan@gmail.com>
2026-08-01 23:50:45 +0530

    find prime in array

--- Introduction/Arrays/arrayPractice.js
+++ Introduction/Arrays/arrayPractice.js
@@ -1,1 +1,24 @@
 console.log("what ever I have done in this week i will do practice of it");
+const Arrays = {
+    isPrime(num){
+        if(num<2){
+            return false;
+        }
+        for(let i=2;i*i<=num;i++){
+         if(num%i === 0){
+            return false;
+         }
+        }
+        return true;
+    },
+    primeInArray(arr){
+        let result = []
+        for(let i =0; i<arr.length;i++){
+            if( this.isPrime(arr[i])){
+                result.push(arr[i]);
+            }
+        }
+        return result;
+    }
+}
+console.log(Arrays.primeInArray([3,2,5,8,12,11]));
