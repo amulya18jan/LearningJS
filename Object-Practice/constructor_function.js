0bdc2693f6bd5f1273965576d07b1ddba2d2935d
Amulya Awasthi <amulyaawasth18jan@gmail.com>
2026-08-04 21:44:57 +0530

    Praticing

--- Object-Practice/constructor_function.js
+++ Object-Practice/constructor_function.js
@@ -1,13 +1,18 @@
+// function User(name){
+//     this.name =name;
+//     this.isAdmin =false;
+// }
+// let user = new User("Amulya");
+// if(user.name === "Amulya"){
+//     user.isAdmin = true;
+// }
+// console.log(user.name);
+// console.log(user);
+// user = new User("Aditi");
+// console.log(user.name);
+// console.log(user);
 function User(name){
-    this.name =name;
-    this.isAdmin =false;
+    console.log(!(new.target));
 }
-let user = new User("Amulya");
-if(user.name === "Amulya"){
-    user.isAdmin = true;
-}
-console.log(user.name);
-console.log(user);
-user = new User("Aditi");
-console.log(user.name);
-console.log(user);
+User();
+new User();
