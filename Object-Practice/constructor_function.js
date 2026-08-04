3d839f41cb128859e35c393e2f751868f817ee3f
Amulya Awasthi <amulyaawasth18jan@gmail.com>
2026-08-04 22:11:13 +0530

    Solved calcutor using constructor

--- Object-Practice/constructor_function.js
+++ Object-Practice/constructor_function.js
@@ -16,12 +16,29 @@
 // }
 // User();
 // new User();
-function User(name){
-    if(!(new.target)){
-        return new User(name);
-    }
-    this.name =name;
+// function User(name){
+//     if(!(new.target)){
+//         return new User(name);
+//     }
+//     this.name =name;
+// }
+// let Amulya = User("King");
+// console.log(Amulya);
+// console.log(Amulya.name);
+function Calculator(){
+   this.read=function(){
+    this.a =+(prompt("give your first number"));
+    this.b =+(prompt("give your second number"));
+   }
+   this.sum=function(){
+    return this.a + this.b;
+   }
+   this.mul=function(){
+    return this.a * this.b;
+   }
 }
-let Amulya = User("King");
-console.log(Amulya);
-console.log(Amulya.name);
+let calculator = new Calculator();
+calculator.read();
+
+alert( "Sum=" + calculator.sum() );
+alert( "Mul=" + calculator.mul() );
