28845285e7f799322a77092c71a5b295142ca392
Amulya Awasthi <amulyaawasth18jan@gmail.com>
2026-08-04 22:33:22 +0530

    Solved calcutor using constructor

--- Object-Practice/constructor_function.js
+++ Object-Practice/constructor_function.js
@@ -25,20 +25,33 @@
 // let Amulya = User("King");
 // console.log(Amulya);
 // console.log(Amulya.name);
-function Calculator(){
-   this.read=function(){
-    this.a =+(prompt("give your first number"));
-    this.b =+(prompt("give your second number"));
-   }
-   this.sum=function(){
-    return this.a + this.b;
-   }
-   this.mul=function(){
-    return this.a * this.b;
-   }
-}
-let calculator = new Calculator();
-calculator.read();
+// function Calculator(){
+//    this.read=function(){
+//     this.a =+(prompt("give your first number"));
+//     this.b =+(prompt("give your second number"));
+//    }
+//    this.sum=function(){
+//     return this.a + this.b;
+//    }
+//    this.mul=function(){
+//     return this.a * this.b;
+//    }
+// }
+// let calculator = new Calculator();
+// calculator.read();
 
-alert( "Sum=" + calculator.sum() );
-alert( "Mul=" + calculator.mul() );
+// alert( "Sum=" + calculator.sum() );
+// alert( "Mul=" + calculator.mul() );
+function Accumulator(startingValue){
+    this.value =startingValue;
+this.read =function(){
+    this.value+=+(prompt("Give me the value",0));
+}
+
+}
+let accumulator = new Accumulator(0); // initial value 1
+
+accumulator.read(); // adds the user-entered value
+accumulator.read(); // adds the user-entered value
+
+alert(accumulator.value);
